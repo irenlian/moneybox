c6296b1567ac108c48c6ddd0059fb8877f02b3e7
irenlian <irynalian@stockx.com>
2020-12-25 21:44:14 +0200

    General setup

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+
+const App: React.FC = () => (
+  <div className="root">
+    <main id="content">
+      <div className="toolbar" />
+      <div>Main page</div>
+    </main>
+  </div>
+);
+
+export default App;
